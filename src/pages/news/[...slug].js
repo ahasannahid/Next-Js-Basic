2e8c623d6f767bf5b7d6ae3e313250e5dd00e80d
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-07-19 17:24:09 +0600

    catch all routes

--- src/pages/news/[...slug].js
+++ src/pages/news/[...slug].js
@@ -1,2 +1,13 @@
 //catch all routes
 //[...slug].js file naming conventation. url er nested router por sompurnotuku catch korar jonne use kora hoy.
+
+
+const CatchAllRoutes = () => {
+    return (
+        <div>
+            <h1>This is for filtering</h1>
+        </div>
+    );
+};
+
+export default CatchAllRoutes;
